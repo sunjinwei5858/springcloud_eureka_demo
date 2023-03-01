9cb61e66f74a1e9f615ba6c655437696629ba2e4
sunjinwei <sunjinwei5828@163.com>
2023-03-01 22:40:20 +0800

    搭建eureka server

---
+++ eureka_server/src/main/java/com/sunjinwei/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.sunjinwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @program: com.sunjinwei
+ * @author: sun jinwei
+ * @create: 2023-03-01 22:24
+ * @description:
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
