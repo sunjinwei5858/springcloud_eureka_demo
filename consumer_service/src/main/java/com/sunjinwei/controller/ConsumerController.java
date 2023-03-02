2a36e414b6a3b4d0a3c406c3e1d95077b9d9aa6c
sunjinwei <sunjinwei5828@163.com>
2023-03-02 08:56:49 +0800

    consumer_service: feign默认集成了ribbon，所以无需引入ribbon依赖

--- consumer_service/src/main/java/com/sunjinwei/controller/ConsumerController.java
+++ consumer_service/src/main/java/com/sunjinwei/controller/ConsumerController.java
@@ -3,6 +3,7 @@ package com.sunjinwei.controller;
 import com.sunjinwei.dto.Result;
 import com.sunjinwei.feign.ProductFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConsumerController {
 
+    @Value("${ribbon.ServerListRefreshInterval}")
+    private String ribbonServerListRefreshInterval;
+
     @Autowired
     private ProductFeignClient productFeignClient;
 
     @GetMapping("/consumer")
     public Result getServiceInfo() {
-        return productFeignClient.getServiceInfo();
+
+        Result serviceInfo = productFeignClient.getServiceInfo();
+        serviceInfo.setMsg(serviceInfo.getMsg() + "====" + ribbonServerListRefreshInterval);
+        return serviceInfo;
     }
 }
